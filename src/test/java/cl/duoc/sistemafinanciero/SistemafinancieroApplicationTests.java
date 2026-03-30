9c650216a70d750ba5d3503b2c2d49a4fcf24b62
ProfGabote <profe.gabote@duoc.cl>
2026-03-30 17:28:08 -0300

    inicializa proyecto spring

---
+++ src/test/java/cl/duoc/sistemafinanciero/SistemafinancieroApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.sistemafinanciero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemafinancieroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
