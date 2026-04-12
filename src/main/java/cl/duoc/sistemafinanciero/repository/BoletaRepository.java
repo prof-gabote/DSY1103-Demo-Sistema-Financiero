1433f724e033dfe09fd0660365160a0c363624be
ProfGabote <profe.gabote@duoc.cl>
2026-04-12 16:04:17 -0400

    fix métodos repo

--- src/main/java/cl/duoc/sistemafinanciero/repository/BoletaRepository.java
+++ src/main/java/cl/duoc/sistemafinanciero/repository/BoletaRepository.java
@@ -10,11 +10,7 @@ import cl.duoc.sistemafinanciero.model.Boleta;
 @Repository
 public interface BoletaRepository extends JpaRepository<Boleta, Long> {
 
-    List<Boleta> findAll();
-
     Boleta findByFolio(String folio);
-
-    Boleta save(Boleta boleta);
 
     void deleteByFolio(String folio);
 }
