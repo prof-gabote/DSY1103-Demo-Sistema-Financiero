3e9f1afd9b4fe6edb6c1be1f093ab072082871a7
ProfGabote <profe.gabote@duoc.cl>
2026-04-12 16:07:04 -0400

    fix atributo v2

--- src/main/java/cl/duoc/sistemafinanciero/model/Boleta.java
+++ src/main/java/cl/duoc/sistemafinanciero/model/Boleta.java
@@ -20,7 +20,7 @@ import lombok.NoArgsConstructor;
 public class Boleta {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
     private String folio;
     private String rutProveedor;
     private String glosa;
